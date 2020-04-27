0f3a15084847d60891bf202546ff278cf1db0435
wpho <u.pho@tcu.edu>
2020-04-27 03:27:58 -0500

    intial commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactonline/HogwartsartifactonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsartifactonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsartifactonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactonline/HogwartsartifactonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsartifactonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
